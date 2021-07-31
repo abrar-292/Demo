129d10b96a591a7b95b532f2d40a06ca15bb5561
Abrar Sanandwala <abrar.sanandwala@mindinventory.com>
2021-07-31 14:49:09 +0530

    -> Demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {HttpClientModule} from "@angular/common/http";
+import {ApiService} from "./api.service";
+import {FormControl, Validators} from "@angular/forms";
+import {PageEvent} from "@angular/material/paginator";
+import {NgxSpinnerService} from "ngx-spinner";
+import {debounce, debounceTime} from "rxjs/operators";
 
 declare var $: any;
 
@@ -8,9 +14,61 @@ declare var $: any;
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-    title = 'demo';
+    searchFormControl = new FormControl('', [
+        Validators.required,
+    ]);
+
+    requestPara: any = {};
+    list = [];
+    listLength;
+    filteredStates = [];
+
+    constructor(private http: HttpClientModule, private api: ApiService, private spinner: NgxSpinnerService) {
+    }
 
     ngOnInit() {
-        $('select').selectpicker();
+        this.requestPara.page = 1;
+
+        this.searchFormControl.valueChanges.pipe(debounceTime(300)).subscribe((e) => {
+            if (e.length > 2) {
+                this.requestPara.search = e;
+                this.getMovieList(this.requestPara);
+            } else {
+                this.list = [];
+                this.listLength = '';
+            }
+        })
+    }
+
+
+    getMovieList(data) {
+        this.spinner.show();
+        const apiKey = 'apikey=4897472c';
+        const page = data && data.page ? '&page=' + data.page : '';
+        const search = data && data.search ? '&s=' + data.search : '';
+        const queryString = apiKey + page + search;
+        this.api.getList(queryString).subscribe((re) => {
+            this.spinner.hide();
+            if (re.Response == 'True') {
+                this.list = re.Search
+                this.listLength = re.totalResults;
+                setTimeout(() => {
+                    window.scroll(0, 0);
+                }, 100)
+            } else {
+                alert(re.Error)
+            }
+        })
+    }
+
+    searchMovie(search) {
+        this.requestPara.search = '';
+        this.requestPara.search = search.value;
+        this.getMovieList(this.requestPara);
+    }
+
+    changePage(e: PageEvent) {
+        this.requestPara.page = e.pageIndex + 1;
+        this.getMovieList(this.requestPara);
     }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,45 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {ApiService} from "./api.service";
+import {HttpClientModule} from "@angular/common/http";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatInput, MatInputModule} from "@angular/material/input";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatButtonModule} from "@angular/material/button";
+import {MatCardModule} from "@angular/material/card";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {NgxSpinnerModule} from "ngx-spinner";
+import {MatOptionModule} from "@angular/material/core";
+import {MatAutocompleteModule} from "@angular/material/autocomplete";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgbModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        NgbModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        MatFormFieldModule,
+        ReactiveFormsModule,
+        MatInputModule,
+        MatButtonModule,
+        MatCardModule,
+        MatPaginatorModule,
+        NgxSpinnerModule,
+        MatOptionModule,
+        MatAutocompleteModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
